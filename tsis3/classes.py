06ba50f70f5e21cd572d04e7c1042ede4c5cd0e9
karukaaa <arukhanym.zhaidary@gmail.com>
2023-02-09 09:51:36 +0600

    classes

---
+++ tsis3/classes.py
@@ -0,0 +1,15 @@
+class Strings:
+    def __init__(self):
+        self.my_str = ""
+
+    def get_string(self):
+        self.my_str = input("Enter string: ")
+
+    def print_string(self):
+        print(self.my_str)
+
+
+# str1 = Strings()
+# str1.get_string()
+# str1.print_string()
+
