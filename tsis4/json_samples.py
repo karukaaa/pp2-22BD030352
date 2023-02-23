c4c35e2a017a1b34f3d8c56591e8ac574dedc0f4
karukaaa <arukhanym.zhaidary@gmail.com>
2023-02-23 09:29:25 +0600

    Added json

--- tsis4/json_samples.py
+++ tsis4/json_samples.py
@@ -1,1 +1,16 @@
 import json
+
+with open('sample-data.json', 'r') as read_file:
+    data = json.load(read_file)
+
+
+for a in range(3):
+    for i, k in data["imdata"][a]["l1PhysIf"]["attributes"].items():
+        if i == "dn":
+            print(k, end=" ")
+        if i == "mtu":
+            print(k, end=" ")
+        if i == "speed":
+            print(k, "\n")
+
+
